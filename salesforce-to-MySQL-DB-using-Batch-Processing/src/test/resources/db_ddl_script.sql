d783e556a16edb149c3916f89475d0b1d0256277
pranith <pranith.diddu@gmail.com>
2019-05-21 01:19:35 -0400

    hi

--- salesforce-to-MySQL-DB-using-Batch-Processing/src/test/resources/db_ddl_script.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE contact (
-  first_name varchar(45) NOT NULL,
-  last_name varchar(45) NOT NULL,
-  email varchar(45) NOT NULL,
-  last_modified timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (email)
-);
--- service-orchestration-and-choice-routing/src/main/resources/order_audits.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `order_audits` (
-  `id` varchar(256) NOT NULL,
-  `order_id` varchar(256) DEFAULT NULL,
-  `total_value` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-)
